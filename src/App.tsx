2c37050bf8f7a8e6d2d40ec77b1c37f3d0c25df8
Xenia Levchenko <variellka@gmail.com>
2023-11-14 12:06:01 +0300

    add eslint and stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>123</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>123</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
